de66b4bc08ee5fd0e7e93c6bb6b42c9434a2c6dc
Bob-p- <poes997@gmail.com>
2022-04-07 12:10:31 +0100

    post

--- init.el
+++ init.el
@@ -1,16 +1,21 @@
-;; INIT.EL - memacs
-
-
+;;-----------------------------------------------------------------------
+;; INIT.EL - memacs 
+;;-----------------------------------------------------------------------
 ;; Disabling things
 ;;-----------------------------------------------------------------------
 (menu-bar-mode -1) 
 (toggle-scroll-bar -1) 
-(tool-bar-mode -1) 
-
-
+(tool-bar-mode -1)
+;;-----------------------------------------------------------------------
+;; Enabling things
+;;-----------------------------------------------------------------------
+(require 'evil)
+(evil-mode 1)
+;; mouse in terminal
+(xterm-mouse-mode 1)
+;;-----------------------------------------------------------------------
 ;; Installing things
 ;;-----------------------------------------------------------------------
-
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
@@ -28,22 +33,23 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
 ;; Make evil conform to markdown mode
 (add-to-list 'load-path "~/.emacs.d/plugins/evil-markdown")
 (require 'evil-markdown)
-
-;; mouse in terminal
-(xterm-mouse-mode 1)
-
+;;-----------------------------------------------------------------------
 ;; Custom things
 ;;-----------------------------------------------------------------------
-
 (defun build-blog ()
   ;; run python script ting
   (save-buffer)
   (interactive)
   (shell-command "cd ~/src/wwwwssss/blog && ./gen.py build"))
+
+(defun build-blog-all ()
+  ;; run python script ting
+  (save-buffer)
+  (interactive)
+  (shell-command "cd ~/src/wwwwssss/blog && ./gen.py all"))
 
 (defun quick-git()
   ;; do a quick git commit -am push
@@ -51,9 +57,10 @@
   (interactive)
   (shell-command "git commit -am 'post' && git push")) 
 
-
 ;; Key bindings
 ;;-----------------------------------------------------------------------
 (global-set-key (kbd "C-c b") 'build-blog)
+(global-set-key (kbd "C-c a") 'build-blog-all)
 (global-set-key (kbd "C-c g") 'magit)
 (global-set-key (kbd "C-c u") 'quick-git)
+(global-set-key (kbd "C-x t") 'neotree-toggle)
